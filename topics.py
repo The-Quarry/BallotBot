dffd55e2149edd3828be4062c6fb306233dfa3a8
Nick Mann <nickmann@Nicks-MacBook-Air.local>
2025-06-07 13:36:12 +0100

    Update environment aliases to include wind farm

--- topics.py
+++ topics.py
@@ -7,10 +7,10 @@ aliases = {
     "health": ["health", "healthcare", "medical", "hospital", "mental health", "primary care", "GP", "GPs", "prescription", "loan"],
     "transport": ["transport", "bus", "ferry", "air travel", "aurigny", "travel", "Condor", "airport", "harbour", "Britanny"],
     "taxation": ["tax", "taxes", "taxation", "income tax", "fiscal policy", "GST", "corporate tax", "corporation tax", "TRP", "Property tax"],
-    "environment": ["environment", "climate", "carbon", "sustainability", "green"],
+    "environment": ["environment", "climate", "carbon", "sustainability", "green", "wind farm"],
     "island wide voting": ["island wide voting", "voting system", "electoral reform", "district"],
     "government reform": ["government reform", "states reform", "reforming government", "deputies numbers", "numbers of deputies", "executive government", "political system", "consensus"],
     "population": ["population", "immigration"],
-    "economy": ["growth", "the economy", "GDP", "finance industry", "entrepeneur", "business", "tourism", "finance"],
+    "economy": ["growth", "the economy", "GDP", "finance industry", "entrepeneur", "business", "tourism", "wind farm", "finance"],
 }
 
