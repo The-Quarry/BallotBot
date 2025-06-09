370fc27c5fdcd20712a2483e5ee7f4bd5d3775dd
Nick Mann <nickmann@Nicks-MacBook-Air.local>
2025-06-09 17:47:56 +0100

    sport art environment

--- topics.py
+++ topics.py
@@ -8,10 +8,12 @@ aliases = {
                "Health and Social Services"],
     "transport": ["transport", "bus", "ferry", "air travel", "aurigny", "travel", "Condor", "airport", "harbour", "Britanny", "runway"],
     "taxation": ["tax", "taxes", "taxation", "income tax", "fiscal policy", "GST", "corporate tax", "corporation tax", "TRP", "Property tax"],
-    "environment": ["environment", "climate", "carbon", "sustainability", "green", "wind farm", "the environment"],
+    "environment": ["environment", "climate", "carbon", "sustainability", "green", "wind farm", "the environment", "renewable energy", "renewables", "renewable", "tidal"],
     "island wide voting": ["island wide voting", "voting system", "electoral reform", "district", "island-wide voting", "islandwide voting"],
     "government reform": ["government reform", "states reform", "reforming government", "deputies numbers", "numbers of deputies", "executive government", "political system", "consensus"],
     "population": ["population", "immigration", "the population"],
     "economy": ["growth", "the economy", "GDP", "finance industry", "entrepeneur", "business", "tourism", "wind farm", "the economy", "finance"],
+    "arts": ["art", "culture", "arts commission", "cultural", "creative", "creatives", "museum", "museums", "heritage", "music", "St James"],
+    "sport": ["sports", "sports commission", "active8", "active 8", "footes lane", "activity", "outdoor", "phsyical", "sports tourism", "beau sejour", "sports development"],
 }
 
